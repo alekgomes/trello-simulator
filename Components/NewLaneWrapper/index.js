85a5d4bda0e05920eb182f18bd6dd98f1afd064b
alekgomes <lucas.maistro@gmail.com>
2020-11-22 16:54:28 -0300

    adicionando estilos

--- Components/NewLaneWrapper/index.js
+++ Components/NewLaneWrapper/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useContext } from "react";
 import { LaneContext } from "../../Context/LaneContext";
 
+import styles from "./style.module.scss";
+
 function NewLaneWrapper() {
   const { addNewLane } = useContext(LaneContext);
   const [laneName, setLaneName] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   const createNewLane = (name) => {
     return {
@@ -15,21 +18,30 @@ function NewLaneWrapper() {
   const handleClick = () => {
     const newLane = createNewLane(laneName);
     addNewLane(newLane);
+    setShowForm(false);
     setLaneName("");
   };
 
   return (
     <>
-      <label htmlFor="laneName">
-        Nome da lane
-        <input
-          type="text"
-          id="laneName"
-          value={laneName}
-          onChange={(e) => setLaneName(e.target.value)}
-        />
-        <button onClick={() => handleClick()}>Criar nova lane!</button>
-      </label>
+      {showForm ? (
+        <div className={styles.wrapper}>
+          <input
+            type="text"
+            id="laneName"
+            value={laneName}
+            placeholder="nome da lista"
+            onChange={(e) => setLaneName(e.target.value)}
+          />
+          <br />
+          <button onClick={() => handleClick()}>Criar nova lane!</button>
+          <button onClick={() => setShowForm(!showForm)}>Cancelar</button>
+        </div>
+      ) : (
+        <div className={styles.newLane} onClick={() => setShowForm(!showForm)}>
+          <span>+ Adicionar nova lista</span>
+        </div>
+      )}
     </>
   );
 }
